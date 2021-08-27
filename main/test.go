f0f4f89b62e064e8a52f8ec57ed29da7b66a1ac3
James Alspach <jalspach@shastacoe.org>
2021-08-27 13:10:04 -0700

    Addeed MQTT stubs into coms pacakge

--- main/test.go
+++ main/test.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"log"
+	"net/url"
+	"os"
+	"time"
+
 	"github.com/jalspach/OverWatch/mypackages/coms"
 	"github.com/jalspach/OverWatch/mypackages/leds"
 )
@@ -10,5 +15,22 @@ func main() {
 	leds.Error(3, 3)
 	leds.Test()
 	coms.Tempc()
+
+	uri, err := url.Parse(os.Getenv("CLOUDMQTT_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	topic := uri.Path[1:len(uri.Path)]
+	if topic == "" {
+		topic = "test"
+	}
+
+	go listen(uri, topic)
+
+	client := coms.MQTTconnect("pub", uri)
+	timer := time.NewTicker(1 * time.Second)
+	for t := range timer.C {
+		client.Publish(topic, 0, false, t.String())
+	}
 
 }
--- mypackages/coms/coms.go
+++ mypackages/coms/coms.go
@@ -2,7 +2,11 @@ package coms
 
 import (
 	"fmt"
+	"log"
+	"net/url"
+	"time"
 
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/yryz/ds18b20"
 )
 
@@ -34,7 +38,7 @@ func Tempf() float64 {
 	for _, sensor := range sensors {
 		t, err := ds18b20.Temperature(sensor)
 		if err == nil {
-			f := t/(5/9)+32
+			f := t/(5/9) + 32
 			return f
 		}
 	}
@@ -50,4 +54,36 @@ func Tempsensors() float64 {
 
 	fmt.Printf("sensor IDs: %v\n", sensors)
 	return 0
+}
+
+//MQTT communications and routines
+
+//place holders so as not to delete the import :-) taken from https://www.cloudmqtt.com/docs/go.html and https://www.emqx.com/en/blog/how-to-use-mqtt-in-golang
+func MQTTconnect(clientId string, uri *url.URL) mqtt.Client {
+	opts := createClientOptions(clientId, uri)
+	client := mqtt.NewClient(opts)
+	token := client.Connect()
+	for !token.WaitTimeout(3 * time.Second) {
+	}
+	if err := token.Error(); err != nil {
+		log.Fatal(err)
+	}
+	return client
+}
+
+func MQTTcreateClientOptions(clientId string, uri *url.URL) *mqtt.ClientOptions {
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(fmt.Sprintf("tcp://%s", uri.Host))
+	opts.SetUsername(uri.User.Username())
+	password, _ := uri.User.Password()
+	opts.SetPassword(password)
+	opts.SetClientID(clientId)
+	return opts
+}
+
+func MQTTlisten(uri *url.URL, topic string) {
+	client := connect("sub", uri)
+	client.Subscribe(topic, 0, func(client mqtt.Client, msg mqtt.Message) {
+		fmt.Printf("* [%s] %s\n", msg.Topic(), string(msg.Payload()))
+	})
 }
