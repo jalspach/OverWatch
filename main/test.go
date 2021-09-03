7f7db3d90ea51810d96fbbdf71c5eaa4fab45a5c
James Alspach <jalspach@shastacoe.org>
2021-09-03 15:04:34 -0700

    added some notes

--- main/test.go
+++ main/test.go
@@ -10,5 +10,12 @@ func main() {
 	leds.Test()
 	coms.Tempc()
 	leds.Reset()
+	// Set run types
+	// oneoff - test leds and continue once
+	// continious - test leds once and then continue
+	// service - skip LED test
+	//
+	// grab temp and network test results and report them to MQTT
+	// set the LED's as apropriate for the network test
 
 }
